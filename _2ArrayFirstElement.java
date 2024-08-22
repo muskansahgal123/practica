d12a7f881214e686b95e9a39277052be1e92ff52
muskansahgal123 <muskansahgal74@gmail.com>
2024-08-22 18:39:26 +0530

    Add files via upload

---
+++ _2ArrayFirstElement.java
@@ -0,0 +1,10 @@
+package _2ArrayPractica;
+
+public class _2ArrayFirstElement {
+    public static void main(String[] args) {
+        int arr[]={2,4,5,8,9};
+        int b=arr[0];
+        System.out.println(b);
+    }
+    
+}
