51699dd043ed69793d95307bb5b801883bca8014
muskansahgal123 <muskansahgal74@gmail.com>
2024-08-22 18:40:14 +0530

    Add files via upload

---
+++ _3ArrayLastElement.java
@@ -0,0 +1,11 @@
+package _2ArrayPractica;
+
+public class _3ArrayLastElement {
+    public static void main(String[]args){
+        int arr1[]={4,6,7,8,9};
+        int c=arr1[4];
+        System.out.println(c);
+
+    }
+    
+}
