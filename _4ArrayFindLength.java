ac0248bd99d057692db22c484effbe367691b4e8
muskansahgal123 <muskansahgal74@gmail.com>
2024-08-22 18:41:18 +0530

    Add files via upload

---
+++ _4ArrayFindLength.java
@@ -0,0 +1,10 @@
+package _2ArrayPractica;
+
+public class _4ArrayFindLength {
+    public static void main(String[] args) {
+        int arr[]={4,8,9,7,6};
+        int b=arr.length;
+        System.out.println(b);
+    }
+    
+}
