cfc721b200b2fd4e5039109d47783a3e4107c6c2
muskansahgal123 <muskansahgal74@gmail.com>
2024-08-29 19:47:27 +0530

    Add files via upload

---
+++ _19ArraysumAllelement.java
@@ -0,0 +1,11 @@
+public class _19ArraysumAllelement {
+    
+    public static void main(String[] args) {
+        int arr[]={2, 4, 6, 8};
+        int sum=0;
+        for(int i=0; i<arr.length; i++){
+            sum=sum+arr[i];
+        }
+        System.out.println(sum);
+}
+}
