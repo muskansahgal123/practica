0cf9e02650b470d145ac3b453f8ef214393e1176
muskansahgal123 <muskansahgal74@gmail.com>
2024-09-04 10:45:05 +0530

    Add files via upload

---
+++ _29ArrayMedianNumber.java
@@ -0,0 +1,10 @@
+package _2ArrayPractica;
+//Find the median of the array [1, 3, 5, 7, 9].
+class Median{
+    public static void main(String[] args) {
+        int arr[]={1,3,5,6,2,3,8,7,9};
+        int median=arr[arr.length/2];
+        System.out.println(median);
+    }
+
+}
