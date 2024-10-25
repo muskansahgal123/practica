7f124ee4fc43e51533d456f8ddeb8b2bdae3ce7c
muskansahgal123 <muskansahgal74@gmail.com>
2024-10-25 17:06:46 +0530

    Add files via upload

--- _10ArrayInsertElementSecondPosition.java
+++ _10ArrayInsertElementSecondPosition.java
@@ -2,20 +2,20 @@ package _2ArrayPractica;
 
 public class _10ArrayInsertElementSecondPosition {
     public static void main(String[] args) {
-        int arr[]={4,5,6,7,8};
-        int arr2[]=new int[arr.length+1];
-        int count=0;
-        for(int i=0; i<arr.length; i++){
-            if(i == 2){
-                arr2[count]=9;
+        int arr[] = { 4, 5, 6, 7, 8 };
+        int arr2[] = new int[arr.length + 1];
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 2) {
+                arr2[count] = 9;
                 count++;
             }
-            arr2[count]=arr[i];
+            arr2[count] = arr[i];
             count++;
         }
-        for(int i=0; i<arr2.length; i++){
+        for (int i = 0; i < arr2.length; i++) {
             System.out.println(arr2[i]);
         }
     }
-    
+
 }
